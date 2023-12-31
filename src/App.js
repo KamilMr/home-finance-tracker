cc59b1e8be7f3d50e28614625857ee1124b4c30b
Kamil Mrowka <kamil@chicos.life>
2023-12-31 12:38:12 +0100

    Add cats

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
 const App = () => {
+  const [catData, setCatData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://cataas.com/cat?json=true')
+      .then(response => response.json())
+      .then(data => {
+        setCatData(data);
+      })
+      .catch(error => console.error('Error fetching data: ', error));
+  }, []);
+
+  // Styles for centering the image
+  const imageContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  };
+
   return (
-    <h1>Robaczek</h1>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Robaczek</h1>
+      {catData && (
+        <div style={imageContainerStyle}>
+          <img src={`https://cataas.com/cat/${catData._id}`} alt="Random Cat" style={{ maxWidth: '100%', height: 'auto' }} />
+          <p>Tags: {catData.tags.join(', ')}</p>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
