81e91cefca21ece8f3154fde31090049f764f6c6
Kamil Mrowka <kamil@chicos.life>
2024-02-17 19:01:18 +0100

    Rm missing

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector} from 'react-redux';
-import {Link, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 import {AppBar, Button, Box, Container} from '@mui/material';
 
