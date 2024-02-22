2dc35f7aa1fca04ec0f0b5494aeff0bc5d1ff88d
Kamil Mrowka <kamil@chicos.life>
2024-02-22 20:45:52 +0100

    Add components in material

---
+++ src/components/material/TableCell.js
@@ -0,0 +1,6 @@
+import {TableCell, styled} from '@mui/material';
+
+const TableCellc = styled(TableCell)({textAlign: 'center'});
+
+const Custome = (props) => <TableCellc {...props} />;
+export default Custome;
