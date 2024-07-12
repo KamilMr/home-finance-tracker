cfe1e560010a131452a1f85994bdebd61bec7c4d
Kamil Mrowka <kamil@chicos.life>
2024-07-12 19:23:51 +0200

    Fix view

--- src/screens/SummaryChart.js
+++ src/screens/SummaryChart.js
@@ -1,16 +1,37 @@
+import {useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
+
 import {Bar} from 'react-chartjs-2';
 import {BarElement, CategoryScale, Chart, LinearScale, Tooltip} from 'chart.js';
-import {useSelector} from 'react-redux';
+import {Container} from '@mui/material';
+
 import {aggregateExpenses} from '../store';
-import { useParams } from 'react-router-dom';
 
 Chart.register(BarElement, CategoryScale, LinearScale, Tooltip);
 
+const chartOptions = (period) => {
+  return {
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+        suggestedMax: period === 30 ? 60 : period === 60 ? 100 : null,
+      },
+    },
+  };
+};
+
 const Charts = () => {
   const {param} = useParams();
-  const aggrExpenses = useSelector(aggregateExpenses(param.split('-').reverse().join('/')));
+  const aggrExpenses = useSelector(
+    aggregateExpenses(param.split('-').reverse().join('/')),
+  );
+  const categories = aggrExpenses.map(o => o.name);
   const data = {
-    labels: aggrExpenses.map((c) => c.name),
+    labels: categories,
     datasets: [
       {
         data: aggrExpenses.map((c) => c.v),
@@ -20,7 +41,11 @@ const Charts = () => {
       },
     ],
   };
-  return <Bar data={data} />;
+  return (
+    <Container sx={{overflowX: 'auto'}}>
+      <Bar data={data} options={chartOptions()} height="400%"/>
+    </Container>
+  );
 };
 
 export default Charts;
